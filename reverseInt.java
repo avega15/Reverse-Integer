afa25668fa21cbd56da188d6915dbaa79f224e96
Abe Vega <avega15@calstatela.edu>
2020-07-03 21:44:59 -0700

    added comments

--- reverseInt.java
+++ reverseInt.java
@@ -3,6 +3,7 @@
 //Print Fizz if divisible by 3, Buzz if divisible by 5, FizzBuzz if divisible my 3 and 5!
 public class reverseInt{
 	public static void main(String[] args) {
+		//This can be any int
 		int x = 1024634;
 		
 		System.out.println("Original int is: " + x);
@@ -12,8 +13,14 @@ public class reverseInt{
 	
 	
 	public static int reverseInt(int x) {
+		//Placeholder int
 		int reverse = 0;
 		
+		//Go through each digit and place x's tenth place into reverse
+		//and removes the tenth place from x.
+		
+		//Every loop adds x's tenth place to reverse
+		//and reverse multiplies by 10 to keep the order reversed. 
 		while(x != 0){
 			reverse = reverse * 10 + x % 10;
 			x = x / 10;
